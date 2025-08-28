1b71594676b6210cc157ac6e529655c08b233f60
Steven Hong <dmt@Stevens-MacBook-Pro.local>
2025-08-27 22:24:05 -0400

    Add working pages structure

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>trakie.ai Upload System</h1>
+      <p>Scan QR code from extension to upload files</p>
+    </div>
+  );
+}
---
+++ pages/sessions/[token].js
@@ -0,0 +1,13 @@
+export default function SessionUpload() {
+  return (
+    <div style={{ 
+      padding: '40px', 
+      textAlign: 'center', 
+      background: '#000', 
+      color: 'white', 
+      minHeight: '100vh' 
+    }}>
+      <h1>Upload Page Works!</h1>
+    </div>
+  );
+}
