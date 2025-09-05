0137f0c99baddf171cdb6ccd0590473a04a1463e
dmtsteve <sebumhong35@gmail.com>
2025-09-04 20:56:13 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center' }}>
-      <h1>trakie.ai Upload System</h1>
-      <p>Scan QR code from extension to upload files</p>
-    </div>
-  );
-}
